92d201d161b6bbee85feb37c2bba315308476c1f
yasminadaileh1 <60566993+yasminadaileh1@users.noreply.github.com>
2020-07-08 01:12:52 +0300

    create branch

--- src/components/hook/axios.js
+++ src/components/hook/axios.js
@@ -1,26 +1,21 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 let url = 'https://todo-app-server-lab32.herokuapp.com/api/v1/todo';
-
+let config = {
+    headers: { 'Content-Type': 'application/json' },
+    mode: 'cors',
+};
 function useAjax(cb) {
     const [items, setItems] = useState([]);
 
     
     const get = async () => {
-        let config = {
-            headers: { 'Content-Type': 'application/json' },
-            mode: 'cors',
-        };
         let response = await axios.get(url, config);
         cb(response.data);
     };
 
     const post = async (item) => {
         item.complete = false;
-        let config = {
-            headers: { 'Content-Type': 'application/json' },
-            mode: 'cors',
-        };
         let data = {
             item: item.item,
             difficulty: item.difficulty,
@@ -33,10 +28,6 @@ function useAjax(cb) {
     };
 
     const update = async (item, _id) => {
-        let config = {
-            headers: { 'Content-Type': 'application/json' },
-            mode: 'cors',
-        };
         url = `${url}/${_id}`;
         let data = {
             item: item.item,
@@ -50,10 +41,6 @@ function useAjax(cb) {
     };
 
     const deleted = async (_id) => {
-        let config = {
-            headers: { 'Content-Type': 'application/json' },
-            mode: 'cors',
-        };
         url = `${url}/${_id}`;
         await axios.delete(url, config);
         let response = await get();
