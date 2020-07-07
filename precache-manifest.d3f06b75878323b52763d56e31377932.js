5347acaa306b3d554b5f63492407f369e16ce49f
yasminadaileh1 <runner@fv-az71.5d0wstjyghwexl5b3xqbkvp2wg.bx.internal.cloudapp.net>
2020-07-07 09:33:46 +0000

    Updates

---
+++ precache-manifest.d3f06b75878323b52763d56e31377932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a660049815d1c6a19601569d8d5662",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f76f75dfb027daca6a4c",
+    "url": "/todo/static/css/main.b06360a0.chunk.css"
+  },
+  {
+    "revision": "cef9475dadc3a0562113",
+    "url": "/todo/static/js/2.253bed51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.253bed51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76f75dfb027daca6a4c",
+    "url": "/todo/static/js/main.604759cc.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
